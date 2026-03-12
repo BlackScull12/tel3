e6a977e544499f52ef0b7691a897a24142ba4968
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 18:40:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ orderBy
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "YOUR_SENDER_ID",
-appId: "YOUR_APP_ID"
+apiKey: "AIzaSyAxt94UyMn8AP8PFaSHPJ29JnZQ2KI3kZw",
+authDomain: "chatgithub-e838d.firebaseapp.com",
+projectId: "chatgithub-e838d",
+storageBucket: "chatgithub-e838d.firebasestorage.app",
+messagingSenderId: "755589384017",
+appId: "1:755589384017:web:6af4c6d223d646cf36f570"
 };
 
 const app = initializeApp(firebaseConfig);
